85f7731f9efa7340c8b378d0be3257eceafe8c69
NandoPla <fernandopla8@gmail.com>
2021-07-29 18:08:09 -0300

    feat: exercicios de JS

---
+++ index.js
@@ -0,0 +1,28 @@
+var firstName = ("Fernando");
+var lastName = ("Pla");
+var age = ("22");
+var food = ("bean, rice, steak and fries");
+
+var fullName = (firstName+" "+lastName);
+var taNaHora = ("It's Time");
+
+console.log(typeof(firstName));
+console.log(firstName);
+
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+var numero = "80";
+var unidade = "kg"
+var peso = numero + unidade;
+var pesoPorDois = peso / 2;
+var duzentos = "200";
+var soma = (duzentos+" "+50);
+var incremento = (5 + incremento); 
+
+console.log(pesoPorDois);
+console.log(soma);
+console.log(incremento);
+
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
